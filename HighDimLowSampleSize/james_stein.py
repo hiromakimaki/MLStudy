a6d02b6f7bf74b05f0a20a5a5d52be33a7e07e0f
hiromakimaki <masayuki1176@gmail.com>
2019-05-12 22:46:55 +0900

    Add true losses

--- HighDimLowSampleSize/james_stein.py
+++ HighDimLowSampleSize/james_stein.py
@@ -42,8 +42,12 @@ def get_param(base_num, d):
 def simulation(base_num, dims, n_simulations):
     ls_mse = np.zeros(len(dims)) # Least squares estimator
     js_mse = np.zeros(len(dims)) # James-Stein estimator
+    ls_true_mse = np.zeros(len(dims)) # d
+    js_true_mse = np.zeros(len(dims)) # d - (d - 2)^2 * E[1 / (d - 2 + 2 * K)], (K ~ Poisson(|theta|^2 / 2))
     for i, d in enumerate(dims):
         theta = get_param(base_num, d)
+        ls_true_mse[i] = d
+        js_true_mse[i] = d - ((d - 2)**2) * np.mean(1 / (d - 2 + 2 * np.random.poisson((np.linalg.norm(theta)**2) / 2, 10000)))
         ls_squared_error = np.zeros(n_simulations)
         js_squared_error = np.zeros(n_simulations)
         for n in range(n_simulations):
@@ -54,17 +58,19 @@ def simulation(base_num, dims, n_simulations):
             js_squared_error[n] = np.linalg.norm(theta - js_theta)**2
         ls_mse[i] = ls_squared_error.mean()
         js_mse[i] = js_squared_error.mean()
-    return ls_mse, js_mse
+    return ls_mse, js_mse, ls_true_mse, js_true_mse
 
 
 def main():
     dims = [3, 10, 20, 30]
     n_simulations = 100
     for base_num in [0, 0.9, 1, 1.1]:
-        ls_mse, js_mse = simulation(base_num, dims, n_simulations)
+        ls_mse, js_mse, ls_true_mse, js_true_mse = simulation(base_num, dims, n_simulations)
         plt.plot(dims, np.zeros(len(dims)), linestyle='--', c='black')
-        plt.plot(dims, ls_mse, label='Least squares')
-        plt.plot(dims, js_mse, label='James-Stein')
+        plt.plot(dims, ls_mse, label='Least squares (Simulated)', linestyle='-', c='blue')
+        plt.plot(dims, js_mse, label='James-Stein (Simulated)', linestyle='-', c='orange')
+        plt.plot(dims, ls_true_mse, label='Least squares (True)', linestyle='--', c='blue')
+        plt.plot(dims, js_true_mse, label='James-Stein (True)', linestyle='--', c='orange')
         plt.legend(loc='upper left')
         plt.title('MSE in the case: base_num = {}'.format(base_num))
         plt.show()
