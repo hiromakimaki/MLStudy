5de2a69d2f5c5771844c2a0dc68205808a6bbafd
hiromakimaki <masayuki1176@gmail.com>
2019-05-17 23:59:31 +0900

    Change test case

--- HypothesisTesting/t_test.py
+++ HypothesisTesting/t_test.py
@@ -34,21 +34,23 @@ def two_sample_t_test():
             (mu_x = 10, mu_y = 10, v_x = 1, v_y = 1)
         Null hypothesis: `mu_x = mu_y`
     """
+    n_iter = 1000
     nx = 50
     ny = 60
-    xs = np.random.normal(10, 1, nx)
-    ys = np.random.normal(10, 1, ny)    
-    t_value = (np.mean(xs) - np.mean(ys)) / np.sqrt((1/nx + 1/ny) * ((nx - 1) * (np.std(xs)**2) + (ny - 1) * (np.std(ys)**2)) / (nx + ny - 2))
-    print('(Mean) Class 1: {}, Class 2: {}'.format(np.mean(xs), np.mean(ys)))
-    print('(Std.) Class 1: {}, Class 2: {}'.format(np.std(xs), np.std(ys)))
-    print('T-value: {}'.format(t_value))
-    alpha = 0.05 / 2
-    lower, upper = stats.t.ppf(q=[alpha, 1-alpha], df=(nx + ny - 2))
-    print(lower, upper)
-    if lower <= t_value <= upper:
-        print('Not rejected.')
-    else:
-        print('Rejected.')
+    alpha = 0.05
+    print('Alpha: {}'.format(alpha))
+    t_values = np.zeros(n_iter)
+    rejected = np.zeros(n_iter)
+    for i in range(n_iter):
+        xs = np.random.normal(10, 1, nx)
+        ys = np.random.normal(10, 1, ny)    
+        t_value = (np.mean(xs) - np.mean(ys)) / np.sqrt((1/nx + 1/ny) * ((nx - 1) * (np.std(xs)**2) + (ny - 1) * (np.std(ys)**2)) / (nx + ny - 2))
+        t_values[i] = t_value
+        lower, upper = stats.t.ppf(q=[alpha/2, 1-alpha/2], df=n)
+        if t_value < lower or upper < t_value:
+            rejected[i] = 1
+    print('Null hypothesis rejected: {} / {}'.format(rejected.sum(), len(rejected)))
+
 
 
 def main():
