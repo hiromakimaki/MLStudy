55f373dbc9a4592949e62915ca7fb91f542230f0
hiromakimaki <masayuki1176@gmail.com>
2019-05-19 01:53:07 +0900

    Add Welch's t test

--- HypothesisTesting/Mean/two_sample.py
+++ HypothesisTesting/Mean/two_sample.py
@@ -23,6 +23,27 @@ def t_test(xs, ys, alpha):
     return 0
 
 
+def welch_t_test(xs, ys, alpha):
+    """
+    Summary:
+        Execute two-sample two-sided Welch's t-test.
+        Same variance assumption is NOT supposed.
+    Return value:
+        0: The null hypothesis is NOT rejected.
+        1: Rejected.
+    """
+    nx = len(xs)
+    ny = len(ys)
+    vx = np.std(xs, ddof=1)**2
+    vy = np.std(ys, ddof=1)**2
+    t_value = (np.mean(xs) - np.mean(ys)) / np.sqrt(vx/nx + vy/ny)
+    df = ((vx/nx + vy/ny)**2) / ((vx/nx)**2 / (nx - 1) + (vy/ny)**2 / (ny - 1))
+    lower, upper = stats.t.ppf(q=[alpha/2, 1-alpha/2], df=df)
+    if t_value < lower or upper < t_value:
+        return 1
+    return 0
+
+
 class TestCase:
     @property
     def alpha(self):
@@ -70,21 +91,27 @@ class CaseDiffMeanSameVar(TestCase):
         return np.random.normal(3, 1, sample_size)
 
 
-def simulation(test_case, n_iter=1000):
+def simulation(test_method, test_case, n_iter=1000):
     rejected = np.zeros(n_iter)
     for i in range(n_iter):
         xs = test_case.sampling_1(test_case.sample_size_1)
         ys = test_case.sampling_2(test_case.sample_size_2)
-        rejected[i] = t_test(xs, ys, test_case.alpha)
+        rejected[i] = test_method(xs, ys, test_case.alpha)
     return rejected
 
 
 def main():
-    rejected = simulation(CaseSameMeanSameVar())
+    rejected = simulation(t_test, CaseSameMeanSameVar())
     print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
-    rejected = simulation(CaseSameMeanDiffVar())
+    rejected = simulation(t_test, CaseSameMeanDiffVar())
     print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
-    rejected = simulation(CaseDiffMeanSameVar())
+    rejected = simulation(t_test, CaseDiffMeanSameVar())
+    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
+    rejected = simulation(welch_t_test, CaseSameMeanSameVar())
+    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
+    rejected = simulation(welch_t_test, CaseSameMeanDiffVar())
+    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
+    rejected = simulation(welch_t_test, CaseDiffMeanSameVar())
     print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
 
 
