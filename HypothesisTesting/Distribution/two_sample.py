ffb17ad19f7d2d13ef939a891dcfee79371c57e4
hiromakimaki <masayuki1176@gmail.com>
2019-05-28 23:36:27 +0900

    Mann-Whitney U test

--- HypothesisTesting/Distribution/two_sample.py
+++ HypothesisTesting/Distribution/two_sample.py
@@ -32,6 +32,25 @@ def birnbaum_tingey_test(xs, ys, alpha):
     return 0
 
 
+def mann_whitney_u_test(xs, ys, alpha):
+    nx = len(xs)
+    ny = len(ys)
+    ranks = stats.rankdata(np.concatenate([xs, ys]))
+    rx = ranks[:nx].sum()
+    ry = ranks[nx:].sum()
+    ux = nx * ny + nx * (nx + 1) / 2 - rx
+    uy = nx * ny + ny * (ny + 1) / 2 - ry
+    u = np.min([ux, uy])
+    # Large sample approximation
+    mu = nx * ny / 2
+    v = nx * ny * (nx + ny + 1) / 12
+    t = (u - mu) / np.sqrt(v)
+    lower, upper = stats.norm.ppf(q=[alpha/2, 1-alpha/2])
+    if t < lower or upper < t:
+        return 1
+    return 0
+
+
 class TestCase:
     @property
     def alpha(self):
@@ -61,13 +80,22 @@ class SameNormalCase(TestCase):
         return self.sampling_1(sample_size)
 
 
-class DiffNormalCase(TestCase):
+class DiffVarNormalCase(TestCase):
 
     def sampling_1(self, sample_size):
         return np.random.normal(2, 4, sample_size)
 
     def sampling_2(self, sample_size):
         return np.random.normal(2, 8, sample_size)
+
+
+class DiffMeanNormalCase(TestCase):
+
+    def sampling_1(self, sample_size):
+        return np.random.normal(0, 4, sample_size)
+
+    def sampling_2(self, sample_size):
+        return np.random.normal(2, 4, sample_size)
 
 
 class NormalLaplaceCase(TestCase):
@@ -88,24 +116,21 @@ class NormalCauchyCase(TestCase):
         return np.random.standard_cauchy(sample_size)
 
 
-def simulation(test_case, n_iter=500):
+def simulation(test_case, test_method, n_iter=500):
     rejected = np.zeros(n_iter)
     for i in range(n_iter):
         xs = test_case.sampling_1(test_case.sample_size_1)
         ys = test_case.sampling_2(test_case.sample_size_2)
-        rejected[i] = birnbaum_tingey_test(xs, ys, test_case.alpha)
+        rejected[i] = test_method(xs, ys, test_case.alpha)
     return rejected
 
 
 def main():
-    rejected = simulation(SameNormalCase())
-    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
-    rejected = simulation(DiffNormalCase())
-    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
-    rejected = simulation(NormalLaplaceCase())
-    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
-    rejected = simulation(NormalCauchyCase())
-    print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
+    for test_case in [SameNormalCase(), DiffVarNormalCase(), DiffMeanNormalCase(), NormalLaplaceCase(), NormalCauchyCase()]:
+        rejected = simulation(test_case, birnbaum_tingey_test)
+        print('Null hypothesis rejected ratio (Birnbaum-Tingey): {} %'.format(100 * sum(rejected) / len(rejected)))
+        rejected = simulation(test_case, mann_whitney_u_test)
+        print('Null hypothesis rejected ratio (Mann-Whitney): {} %'.format(100 * sum(rejected) / len(rejected)))
 
 
 if __name__=='__main__':
