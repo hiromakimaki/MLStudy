5c795abc9a534abec983689e4b40fcf1e6c26640
hiromakimaki <masayuki1176@gmail.com>
2019-06-06 23:45:56 +0900

    Auto correlation

--- HypothesisTesting/TimeSeries/no_auto_corr.py
+++ HypothesisTesting/TimeSeries/no_auto_corr.py
@@ -1,4 +1,22 @@
+import numpy as np
+
+
+def auto_corr(xs, m):
+    n = len(xs)
+    assert n > m
+    mu = np.mean(xs)
+    auto_corrs = np.zeros(m+1)
+    for lag in range(m+1):
+        ys = xs[:n-lag]
+        zs = xs[lag:]
+        auto_corrs[lag] = np.mean((ys - mu) * (zs  - mu))
+    auto_corrs = auto_corrs / auto_corrs[0]
+    return auto_corrs
+
+
 def main():
+    xs = np.random.normal(0, 5, 20)
+    print(auto_corr(xs, 10))
     pass
 
 if __name__=='__main__':
