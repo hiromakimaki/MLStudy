c525c2d2d77e92956b7c507792223bd5a52e2a81
hiromakimaki <masayuki1176@gmail.com>
2019-06-13 00:22:54 +0900

    Use t-statistics

--- HypothesisTesting/TimeSeries/unit_root.py
+++ HypothesisTesting/TimeSeries/unit_root.py
@@ -35,24 +35,32 @@ def main():
     print('Null hypothesis rejected ratio: {} %'.format(100 * sum(rejected) / len(rejected)))
 
 
+def t_stat(xs, rho):
+    n = len(xs)
+    est_rho = ols_estimator(xs)
+    s2 = ((xs[1:] - est_rho * xs[:-1])**2).mean()
+    sigma = np.sqrt(s2 / (xs[:-1]**2).sum())
+    return (est_rho - rho) / sigma
+
+
 def check_distribution(n_iter=500):
-    tau_x = np.zeros(n_iter)
-    tau_y = np.zeros(n_iter)
-    tau_z = np.zeros(n_iter)
+    est_rho_x = np.zeros(n_iter)
+    est_rho_y = np.zeros(n_iter)
+    est_rho_z = np.zeros(n_iter)
     rho_x = 0
     rho_y = 0.5
     rho_z = 1
     n = 500
     for i in range(n_iter):
         xs = generate_ar1_data(n, rho_x) # not unit root process
-        tau_x[i] = np.sqrt(n) * (ols_estimator(xs) - rho_x)
+        est_rho_x[i] = t_stat(xs, rho_x)
         ys = generate_ar1_data(n, rho_y) # not unit root process
-        tau_y[i] = np.sqrt(n) * (ols_estimator(ys) - rho_y)
-        zs = generate_ar1_data(n, rho_y) # unit root process
-        tau_z[i] = np.sqrt(n) * (ols_estimator(zs) - rho_z)
-    plt.hist(xs, bins=20, alpha=0.3, label='x')
-    plt.hist(ys, bins=20, alpha=0.3, label='y')
-    plt.hist(zs, bins=20, alpha=0.3, label='z')
+        est_rho_y[i] = t_stat(ys, rho_y)
+        zs = generate_ar1_data(n, rho_z) # unit root process
+        est_rho_z[i] = t_stat(zs, rho_z)
+    plt.hist(est_rho_x, bins=20, alpha=0.3, label='x')
+    plt.hist(est_rho_y, bins=20, alpha=0.3, label='y')
+    plt.hist(est_rho_z, bins=20, alpha=0.3, label='z')
     plt.legend(loc='upper left')
     plt.show()
 
