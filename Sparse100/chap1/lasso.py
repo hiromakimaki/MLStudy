5fdd28f2f077c887e90d789a7d33643de391ea78
hiromakimaki <masayuki1176@gmail.com>
2021-06-01 03:23:53 +0900

    add lasso path

--- Sparse100/chap1/lasso.py
+++ Sparse100/chap1/lasso.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 np.random.seed(0)
@@ -40,16 +41,29 @@ class MyLinearLasso:
 def generate_sample_data():
     n, p = 100, 5
     X = np.random.normal(0, 1, n * p).reshape((n, p))
-    beta = np.arange(p)
+    beta_sign = (-1) ** np.arange(p)
+    beta = beta_sign * np.arange(1, p + 1)
     y = np.dot(X, beta)
     return X, y
 
 
 def main():
-    model = MyLinearLasso(alpha=1.0, fit_intercept=False)
     X, y = generate_sample_data()
-    beta = model.fit(X, y)
-    print(beta)
+    n, p = X.shape
+    alphas = np.arange(0.05, 5, 0.05)
+    betas = np.zeros((alphas.shape[0], p))
+    for i, alpha in enumerate(alphas):
+        print(f'{i}-th iteration started.')
+        model = MyLinearLasso(alpha=alpha, fit_intercept=False)
+        beta = model.fit(X, y)
+        betas[i, :] = beta.copy()
+    for j in range(p):
+        plt.plot(alphas, betas[:, j], label=f'$\\beta_{j}$')
+    plt.legend(loc='upper right')
+    plt.xlabel(r'$\alpha$')
+    plt.ylabel(r'$\beta$')
+    plt.title('LASSO path')
+    plt.show()
 
 
 if __name__ == '__main__':
